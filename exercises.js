1df69f0925c4a63bb1209a0bc0fdc9df40f00d6e
Slava <49091983+SlavaKittel@users.noreply.github.com>
2020-06-11 00:44:05 +0300

    2020.06.11

--- exercises.js
+++ exercises.js
@@ -1,11 +1,10 @@
-
 // OLD CODE.MU
 
 ////////  LESSON 2  ////////
 
 //// EXERCISE 1
 
-const str = 'abcde';
+const str = "abcde";
 console.log(str[0]);
 console.log(str[1]);
 console.log(str[2]);
@@ -15,7 +14,7 @@ console.log(str[4]);
 //// EXERCISE 2
 
 const hours = 9;
-const howSecondsInHour = hours*360;
+const howSecondsInHour = hours * 360;
 
 console.log(howSecondsInHour + "s");
 
@@ -26,18 +25,18 @@ num += 12;
 num -= 14;
 num *= 5;
 num /= 2.5;
-num++;  // num += 1 === num = num + 1
-num--;  // num -= 1 === num = num - 1
+num++; // num += 1 === num = num + 1
+num--; // num -= 1 === num = num - 1
 console.log(num);
 
 //// Work with variables
 
 const a = 10;
 const b = 2;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
 
 const c = 15;
 const d = 2;
@@ -50,17 +49,17 @@ console.log(res);
 //// Work with strings
 
 const string = "Hello World!";
-const string2 ="How are U?";
+const string2 = "How are U?";
 
 console.log(string + string2);
 
 const name = "Slava";
-console.log('Hi,' + ' ' + name);
+console.log("Hi," + " " + name);
 
 //// String characters(obrashenie k simvolam stroki)
 
-const num1 = '12345';
-console.log(num1[0]*num1[1]*num1[2]*num1[3]*num1[4]);
+const num1 = "12345";
+console.log(num1[0] * num1[1] * num1[2] * num1[3] * num1[4]);
 
 //// prompt
 
@@ -89,33 +88,42 @@ console.log(num);
 
 //// WORK WITH ARRAYS
 
-arr = ['a', 'b' , 'c' , 'd']
+arr = ["a", "b", "c", "d"];
 console.log(arr);
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
-console.log(arr[0] + '+' + arr[1],arr[2] + '+' + arr[3]);
+console.log(arr[0] + "+" + arr[1], arr[2] + "+" + arr[3]);
 
 arrNumbers = [2, 5, 3, 9];
 
-const resultNumbers = arrNumbers[0] * arrNumbers[1] + arrNumbers[2] * arrNumbers[3];
+const resultNumbers =
+  arrNumbers[0] * arrNumbers[1] + arrNumbers[2] * arrNumbers[3];
 
 console.log(resultNumbers);
 
 //// ASSOCIATED OBJECT
 
-var obj = {a: 1, b: 2, c: 3};
-console.log(obj['c']);
+var obj = { a: 1, b: 2, c: 3 };
+console.log(obj["c"]);
 
 var objSalary = { kolja: "1000", Jack: "2500", Lois: "1750" };
 
 console.log(objSalary["Lois"]);
 
-const day1 = 1
+const day1 = 1;
 
-var objWeek = { day1: "Mon", 2: "Tues", 3: "Wed", 4: "Thur", 5: "Fri", 6: "Sat", 7: "Sun" };
+var objWeek = {
+  day1: "Mon",
+  2: "Tues",
+  3: "Wed",
+  4: "Thur",
+  5: "Fri",
+  6: "Sat",
+  7: "Sun",
+};
 
-console.log(objWeek['day1']);
+console.log(objWeek["day1"]);
 
 //// Multi-dimensional arrays
 
@@ -129,10 +137,10 @@ console.log(multiArr[1][0]);
 
 const multiObj = { js: ["jQuery", "Angular"], php: "hello", css: "world" };
 
-console.log(multiObj['js'][0]);
+console.log(multiObj["js"][0]);
 
 const lang = {
-  'ru': {
+  ru: {
     day: "Пон",
     // (2: "Втор"),
     // (3: "Сред"),
@@ -141,7 +149,7 @@ const lang = {
     // (6: "Суб"),
     // (7: "Воск"),
   },
-  'en': {
+  en: {
     day1: "Mon",
     // (2: "Tues"),
     // (3: "Wed"),
@@ -152,7 +160,7 @@ const lang = {
   },
 };
 
-console.log(lang['en']['day1']);
+console.log(lang["en"]["day1"]);
 
 ////////  LESSON 4  ////////
 
@@ -160,18 +168,19 @@ console.log(lang['en']['day1']);
 
 const t = 5;
 
-if (t == 0) {   // одиночное равно зарезервировано за присваиванием.
-                //   На самом деле оно работает так:
-                //   переменной t присвоить 0,
-                //   если удалось присвоить - то выведи 'верно',
-                //   иначе (если не удалось присвоить) выведи 'неверно'.
-                // if (t = 0) alert('Верно!'); else alert('Неверно!'); всегда будет выводить 'Верно!'
+if (t == 0) {
+  // одиночное равно зарезервировано за присваиванием.
+  //   На самом деле оно работает так:
+  //   переменной t присвоить 0,
+  //   если удалось присвоить - то выведи 'верно',
+  //   иначе (если не удалось присвоить) выведи 'неверно'.
+  // if (t = 0) alert('Верно!'); else alert('Неверно!'); всегда будет выводить 'Верно!'
   console.log("true");
 } else {
-  console.log("false");;
+  console.log("false");
 }
 
-if (t > 0){
+if (t > 0) {
   console.log("true");
 } else {
   console.log("false");
@@ -183,29 +192,29 @@ if (t < 0) {
   console.log("false");
 }
 if (t >= 0) {
-  console.log('true');
+  console.log("true");
 } else {
-  console.log('false');
+  console.log("false");
 }
 
 if (t <= 0) {
-  console.log('true');
+  console.log("true");
 } else {
-  console.log('false');
+  console.log("false");
 }
 
 if (t != 0) {
-  console.log('true');
+  console.log("true");
 } else {
-  console.log('false');
+  console.log("false");
 }
 
 console.log(typeof arr);
 
-if (t == 'test') {
-  console.log('true');
+if (t == "test") {
+  console.log("true");
 } else {
-  console.log('false');
+  console.log("false");
 }
 
 const test = false;
@@ -224,17 +233,17 @@ if (test !== true) {
   console.log(false);
 }
 
-if (t !== 3) console.log('pisja'); 
-else console.log('sisja');
+if (t !== 3) console.log("pisja");
+else console.log("sisja");
 
 const g = 3;
 const h = 5;
 
 if (g > 0 && g < 5) {
-  console.log('yes');
+  console.log("yes");
 } else {
-  console.log('no');
-} 
+  console.log("no");
+}
 
 if (g == 0 || g == 2) {
   console.log(g + 7);
@@ -248,10 +257,10 @@ if (g <= 1 && h >= 3) {
   console.log(g - h);
 }
 
-if (g > 2 && g < 11 || h >= 6 && h <14) {
-  console.log('yes');
+if ((g > 2 && g < 11) || (h >= 6 && h < 14)) {
+  console.log("yes");
 } else {
-  console.log('no');
+  console.log("no");
 }
 
 //// switch-case
@@ -260,14 +269,14 @@ const num2 = 1;
 
 switch (num2) {
   case 1:
-    console.log('spring');
+    console.log("spring");
     break;
   case 2:
-    console.log('summer');
+    console.log("summer");
     break;
   default:
-    console.log('loh');
-};
+    console.log("loh");
+}
 
 const day = -54;
 if (day > 0 && day < 11) {
@@ -295,14 +304,13 @@ if (stringLesson4[0] == "a" || "b" || "c") {
   console.log("no");
 }
 
-
 const str2 = "123312";
 Number(str2);
 console.log(str2);
 console.log(typeof parseInt(str2[0]));
 console.log(+str2[0] + +str2[1] + +str2[2]);
 
-if (+str2[0] + +str2[1] + +str2[2] == +str2[3] + +str2[4] + +str2[5] ) {
+if (+str2[0] + +str2[1] + +str2[2] == +str2[3] + +str2[4] + +str2[5]) {
   console.log("yes");
 } else {
   console.log("no");
@@ -321,24 +329,22 @@ if (lang2 == "en") {
 console.log(arrLang);
 
 switch (lang2) {
-  case 'ru':
-    arrLang = [2]
+  case "ru":
+    arrLang = [2];
     break;
-  case 'en':
-    arrLang = [4]
+  case "en":
+    arrLang = [4];
     break;
-  }
-
+}
 
 console.log(arrLang);
 
 const arrLang2 = {
-  'ru': ['suka'],
-  'en': ['kuka']
+  ru: ["suka"],
+  en: ["kuka"],
 };
 
 console.log(arrLang2[lang2]);
-
 
 ////////  LESSON 5  ////////
 
@@ -346,8 +352,8 @@ console.log(arrLang2[lang2]);
 
 // let u = 1;
 // while (u <= 100) {
-  // console.log(u);
-  // u++;
+// console.log(u);
+// u++;
 // }
 
 // for (i = 1; i < 101; i++) {
@@ -357,14 +363,14 @@ console.log(arrLang2[lang2]);
 
 // arr with numbers 1 3 5 7 etc.
 
- for (let i = 1; i < 101; i++) { 
+for (let i = 1; i < 101; i++) {
   //  i++ * 2;
   //  console.log(i - 1);
- }
+}
 
 // vavydom vse chisla podrjat iz massiva
 
-const arr10 = [1,2,3,4,5];                    
+const arr10 = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr10.length; i++) {
   console.log(arr10[i]);
 }
@@ -379,17 +385,17 @@ console.log(result2);
 
 //// for-in
 
-let object = {green: 'зеленый', red: 'красный', blue: 'голубой'};
+let object = { green: "зеленый", red: "красный", blue: "голубой" };
 
 for (let key in object) {
   console.log(key);
   console.log(object[key]);
-  console.log(key + ' ' + 'plus' + ' ' + object[key]);
+  console.log(key + " " + "plus" + " " + object[key]);
 }
 
 //// Tasks
 
-// only numbers >3 and <10 in this array 
+// only numbers >3 and <10 in this array
 
 const arr21 = [2, 5, 9, 15, 0, 4];
 for (let i = 0; i < arr21.length; i++) {
@@ -398,84 +404,84 @@ for (let i = 0; i < arr21.length; i++) {
   }
 }
 
-console.log('space');
+console.log("space");
 
 const arr31 = [1, 2, 5, 9, 13, 4, 10];
 for (let i = 0; i < arr31.length; i++) {
   if (arr31[i] == 2) {
-    console.log('yes');
+    console.log("yes");
     break;
   }
 }
 
-console.log('space');
+console.log("space");
 
 const arr41 = [1, 2, -5, 9, -13, -4, 10];
 let result41 = 0;
 for (let i = 0; i < arr41.length; i++) {
-  if (arr41[i] > 0 ) {
+  if (arr41[i] > 0) {
     result41 += arr41[i];
   }
 }
 console.log(result41); // nuzno vyvesti consol log naruzu(inside loop repeat)
 
-console.log('space');
+console.log("space");
 
 //  Дан массив числами, например: [10, 20, 30, 50, 235, 3000].                              !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Выведите на экран только те числа из массива, которые начинаются на цифру 1, 2 или 5.
 
-const arr51 = [10, 20, 30, 50, 235, 3000];                                            // POCHTIIII
+const arr51 = [10, 20, 30, 50, 235, 3000]; // POCHTIIII
 let resultarr51 = [];
 for (let i = 0; i < arr51.length; i++) {
-  if (arr51[i].toString()[0] == (1 || 2)) {  // || ??????
+  if (arr51[i].toString()[0] == (1 || 2)) {
+    // || ??????
     resultarr51 += arr51[i] + " ";
   }
-};
+}
 console.log(resultarr51);
-
 
 const numberTest = 546;
 console.log(numberTest.toString()[0]);
 
-console.log('space');
+console.log("space");
 
-// Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9.                          
+// Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9.
 // С помощью цикла for создайте строку '-1-2-3-4-5-6-7-8-9-'.
 
-const arr61 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-document.write('Exersizes:')
-document.write('<br>')
-document.write('-')
+const arr61 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+document.write("Exersizes:");
+document.write("<br>");
+document.write("-");
 for (let i = 0; i < arr61.length; i++) {
-   document.write(arr61[i] + "-");
+  document.write(arr61[i] + "-");
 }
-document.write('<br>')
-document.write("Write some text directly to the HTML document");   
-console.log('NEXT');
-document.write('<br>')
+document.write("<br>");
+document.write("Write some text directly to the HTML document");
+console.log("NEXT");
+document.write("<br>");
 
-const arrWeek = ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+const arrWeek = ["M", "T", "W", "T", "F", "S", "S"];
 
 for (let i = 0; i < 5; i++) {
   const element = arrWeek[i];
-  document.write(element)
+  document.write(element);
   console.log(element);
 }
 
 for (let i = 5; i < 7; i++) {
   const element = arrWeek[i];
-  document.write(element.bold())
+  document.write(element.bold());
   console.log(element.bold());
 }
 
-console.log('NEXT1');
-document.write('<br>')
+console.log("NEXT1");
+document.write("<br>");
 
 // Составьте массив дней недели. С помощью цикла for                                       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// выведите все дни недели, а текущий день выведите курсивом. 
+// выведите все дни недели, а текущий день выведите курсивом.
 // Текущий день должен храниться в переменной day
 
-const arrWeek2 = ['M', 'T', 'W', 'T', 'F', 'S', 'S']                      
+const arrWeek2 = ["M", "T", "W", "T", "F", "S", "S"];
 const day2 = 2;
 
 for (let i = 0; i < arrWeek2.length; i++) {
@@ -484,35 +490,31 @@ for (let i = 0; i < arrWeek2.length; i++) {
     // console.log(element.italics());
     // document.write(element.italics());
     // element.splice[2, 1];
-  } 
+  }
 
   // console.log(element);
   // document.write(element);
-
 }
 
-
-         
 // const arrE = [1, 2, 3, 4, 5];
 // for (var i = 0; i < arrE.length; i++) {
-//   document.write(arrE[i]);                    
-// } 
+//   document.write(arrE[i]);
+// }
 
-console.log('NEXT2');
-document.write('<br>')
-
+console.log("NEXT2");
+document.write("<br>");
 
 const arr54 = [1, 2, 3, 4, 5];
 for (var i = 0; i < arr54.length; i++) {
-	if (arr[i] === 3) {
-		break; //выходим из цикла
-	} else {
-		console.log(arr54[i]);
-	}
+  if (arr[i] === 3) {
+    break; //выходим из цикла
+  } else {
+    console.log(arr54[i]);
+  }
 }
 
 // Дано число n=1000. Делите его на 2 столько раз, пока результат деления не станет меньше 50.
-// Какое число получится? Посчитайте количество итераций, 
+// Какое число получится? Посчитайте количество итераций,
 // необходимых для этого (итерация - это проход цикла), и запишите его в переменную num.
 
 const n = 1000;
@@ -526,11 +528,11 @@ const n = 1000;
 
 function button() {
   setTimeout(function () {
-    console.log('hi');
+    console.log("hi");
   }, 2000);
-};
+}
 
-// let buttonSetTimeout; 
+// let buttonSetTimeout;
 // buttonSetTimeout.onclick = function () {
 //   console.log('loh');
 // };
@@ -548,7 +550,7 @@ if (result77 == 0) {
   console.log("divide");
 } else {
   console.log("divide with excess:" + result77);
-}  
+}
 
 //
 
@@ -560,25 +562,25 @@ console.log(Math.sqrt(9));
 
 //
 
-const arr77 = [4, 2, 5, 19, 13, 0, 10]
-let elementSum = 0
+const arr77 = [4, 2, 5, 19, 13, 0, 10];
+let elementSum = 0;
 for (let i = 0; i < arr77.length; i++) {
-  elementSum += (arr77[i]**3); 
+  elementSum += arr77[i] ** 3;
 }
 console.log(Math.sqrt(elementSum));
 
 //
 
-let j = 379; // Math.round - true reduction 
+let j = 379; // Math.round - true reduction
 let ceil = Math.ceil(Math.sqrt(j));
-let floor = Math.floor(Math.sqrt(j));;
+let floor = Math.floor(Math.sqrt(j));
 console.log(Math.sqrt(j));
-console.log(floor); 
-console.log(ceil); 
+console.log(floor);
+console.log(ceil);
 
 //
 
-arr88 = [4, -2, 5, 19, -130, 0, 10]
+arr88 = [4, -2, 5, 19, -130, 0, 10];
 
 console.log(Math.max.apply(null, arr88)); // why null??
 console.log(Math.min(4, -2, 5, 19, -130, 0, 10)); // also with max
@@ -586,18 +588,18 @@ console.log(Math.min(4, -2, 5, 19, -130, 0, 10)); // also with max
 console.log(Math.round(Math.random() * 100)); // 0 .. 100 - random value
 
 //
-let arr99 = [];                                         // 1st - add NEW ARRAY with EMPTY value
+let arr99 = []; // 1st - add NEW ARRAY with EMPTY value
 for (let i = 0; i < 10; i++) {
-  arr99[i] = Math.round(Math.random() * 100);           // array[INDEX]
+  arr99[i] = Math.round(Math.random() * 100); // array[INDEX]
 }
 
-console.log(arr99);                                     // OUTSIDE EXECUTION C.L.
+console.log(arr99); // OUTSIDE EXECUTION C.L.
 
 //
 
 let o = 3;
 let p = 9;
-console.log(Math.abs(o+p));
+console.log(Math.abs(o + p));
 //
 
 let l = o - p;
@@ -608,16 +610,16 @@ console.log(Math.abs(l));
 arr55 = [12, 15, 20, 105, 754, -950];
 let resultarr55 = 0;
 for (let i = 0; i < arr55.length; i++) {
-  resultarr55 += arr55[i]; 
+  resultarr55 += arr55[i];
 }
 
-console.log(Math.round(resultarr55/arr55.length));  // arithmetical mean number
+console.log(Math.round(resultarr55 / arr55.length)); // arithmetical mean number
 
 //factorial of a number
 let w = 170;
 
 let factorialOfValueW = 1;
-for (let i = w; (i > 0); i--) {
+for (let i = w; i > 0; i--) {
   factorialOfValueW *= i;
 }
 
@@ -625,14 +627,14 @@ console.log(factorialOfValueW);
 
 ////////  LESSON 7  ////////
 
-//SLICE toUpperCase toLowerCase 
+//SLICE toUpperCase toLowerCase
 
-let str11 = 'js'; 
+let str11 = "js";
 console.log(str11.toUpperCase());
 console.log(str11.toLowerCase());
 
-let str22 = "Where is pisja?"
-if (str22.length > 10 ) {
+let str22 = "Where is pisja?";
+if (str22.length > 10) {
   console.log(str22.slice(0, 10) + "...");
 } else {
   console.log(str22);
@@ -642,19 +644,19 @@ if (str22.length > 10 ) {
 
 let str23 = "I-learn-js-(and)-html!";
 
-console.log(str23.replace("-","!"));    // Match only first position
-console.log(str23.replace(/-/g,"!"));   // GLOBAL MATCH !!!! Replace all occurrences!!
-console.log(str23.replace(/\(/g,"0"));  // Чтобы использовать специальный символ как обычный, добавьте к нему обратную косую черту: \..
-                                        // Это называется «экранирование символа». Списко спец символов: [ \ ^ $ . | ? * + ( )
-console.log(str23.replace(/l/g,"H"));   
+console.log(str23.replace("-", "!")); // Match only first position
+console.log(str23.replace(/-/g, "!")); // GLOBAL MATCH !!!! Replace all occurrences!!
+console.log(str23.replace(/\(/g, "0")); // Чтобы использовать специальный символ как обычный, добавьте к нему обратную косую черту: \..
+// Это называется «экранирование символа». Списко спец символов: [ \ ^ $ . | ? * + ( )
+console.log(str23.replace(/l/g, "H"));
 
 // SPLIT:  string => array
 
 let str33 = "I learn js";
 console.log(str33.split(" ")); // use "space" for split strin on ARRAY
 
-let date = '2020-05-08';
-let dateSplit = date.split('-').reverse().join('.');
+let date = "2020-05-08";
+let dateSplit = date.split("-").reverse().join(".");
 console.log(dateSplit); //08.05.2020
 
 // JOIN:  array => string
@@ -668,12 +670,13 @@ let str55 = "hi loh bloh vaj hello world";
 // let resultStr55 = str55.toUpperCase()[0] + str55.slice(1, Infinity);
 // console.log(resultStr55); // Hi loh
 
-let resultStr55v2 = str55.split(' '); // (3) ["hi", "loh", "bloh"]
+let resultStr55v2 = str55.split(" "); // (3) ["hi", "loh", "bloh"]
 console.log(resultStr55v2);
 let String55V2 = "";
 
 for (let i = 0; i < resultStr55v2.length; i++) {
-  let newResultStr55v2 = resultStr55v2[i].toUpperCase()[0] + resultStr55v2[i].slice(1, Infinity); 
+  let newResultStr55v2 =
+    resultStr55v2[i].toUpperCase()[0] + resultStr55v2[i].slice(1, Infinity);
   String55V2 += newResultStr55v2 + " ";
 }
 console.log(typeof newResultStr55v2); // typeof STRING!!!!!!!!!!
@@ -682,12 +685,13 @@ console.log(String55V2);
 
 // Преобразуйте строку 'var_test_text' в 'varTestText'. Скрипт, конечно же, должен работать с любыми аналогичными строками
 
-let str77 = 'var_test_text';
-let resultStr77 = str77.split('_');  // => array
+let str77 = "var_test_text";
+let resultStr77 = str77.split("_"); // => array
 let answer = resultStr77[0];
 
 for (let i = 1; i < resultStr77.length; i++) {
-  let mainResulrtStr77 = resultStr77[i].toUpperCase()[0] + resultStr77[i].slice(1, Infinity);
+  let mainResulrtStr77 =
+    resultStr77[i].toUpperCase()[0] + resultStr77[i].slice(1, Infinity);
   answer += mainResulrtStr77;
 }
 
@@ -699,7 +703,20 @@ console.log(answer); // 'varTestText'
 
 // arr1.concat(arr2, arr3) // arr1 + arr2
 
-// arr.reverse(); //[1,2,3] => [3,2,1]
+let arrm1 = [1, 2, 3];
+let arrm2 = [4, 8, 6];
+// console.log(arrm1.concat(arrm2));
+// console.log(...arrm1, ...arrm2); // spread operation
 
-// 
+// // arr.reverse(); //[1,2,3] => [3,2,1]
 
+// console.log(arrm2.sort());
+
+// let objArr = { js: "test", jq: "hello", css: "world" };
+
+// console.log(Object.keys(objArr));
+
+console.log(arrm1.splice(1, 0, "a", "b").splice(3,0,"c")); // vyvodit to chto udalit, ne vyvdoit to chto dobavit
+console.log(arrm1); // vyvodit  to chto ostalos
+
+////////  LESSON 9  ////////
