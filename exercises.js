11f875c85318f153cf49f3df145b09a9561df935
Slava <49091983+SlavaKittel@users.noreply.github.com>
2020-06-18 12:35:24 +0300

    6.18.2020

--- exercises.js
+++ exercises.js
@@ -716,7 +716,469 @@ let arrm2 = [4, 8, 6];
 
 // console.log(Object.keys(objArr));
 
-console.log(arrm1.splice(1, 0, "a", "b").splice(3,0,"c")); // vyvodit to chto udalit, ne vyvdoit to chto dobavit
+console.log(arrm1.splice(1, 1, "a", "b"));  // vyvodit to chto udalil, NE VYVODIT!! to chto dobavil
+arrm1.splice(5, 0, "c");
 console.log(arrm1); // vyvodit  to chto ostalos
 
 ////////  LESSON 9  ////////
+
+// Дана строка, например, '123456'. Переверните эту строку (сделайте из нее '654321')
+
+strL9 = "aiouyh123456 akus rdip";
+console.log(strL9.split("").reverse().join(""));   // SPLIT USE WITH "" !!!!!!!!!!!!!!!!!!!
+console.log(strL9[1]);
+
+// Дана строка. Сделайте заглавным первый символ этой строки.
+let strFirstCase = strL9.toUpperCase()[0] + strL9.substr(1); // .substr() = .slice() ==> SLICE(kusochek) WORK WITH STRING!!!!!!! 
+console.log(strFirstCase);
+
+// Проверьте, что строка начинается на http://
+let strIndexOf = 'http://slavakittel.com'
+
+let strTrueFalse = strIndexOf.indexOf('http://', 0);
+
+if (strTrueFalse == 0) {
+  console.log(true);
+} else {
+  console.log(false);
+};
+//second solutuion  - str.substr(0, 7) == 'http://'
+
+// Проверьте, что строка заканчивается на .html.
+
+if (strIndexOf.length - 4 == strIndexOf.indexOf('.com')) {
+  console.log(true);
+} else {
+  console.log(false);
+};
+
+//second solutuion  - str.substr(-5) == 'http://' - if "minus" then get end of string
+
+////////  LESSON 10  ////////
+
+// Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
+
+function square(a) {
+  return a * a
+}
+
+console.log(square(12));
+
+// Сделайте функцию, которая возвращает сумму двух чисел.
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(41, 5));
+
+// Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
+
+function sumPlus(a, b, c) {
+  return (a - b) / c
+}
+
+console.log(sumPlus(8, 2, 2));
+
+// Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
+
+const weekNumber = 4;
+
+function week(weekNumber) {
+  if (weekNumber == 1) {
+    return "monday";
+  }
+  if (weekNumber == 2) {
+    return "tuesday";
+  }
+  if (weekNumber == 3) {
+    return "wendsday";
+  }
+  if (weekNumber == 4) {
+    return "thursday";
+  }
+  if (weekNumber == 5) {
+    return "friday";
+  }
+  if (weekNumber == 6) {
+    return "saturday";
+  }
+  if (weekNumber == 7) {
+    return "sunday";
+  } else {
+    return "input from 1 to 7"
+  }
+}
+
+console.log(week(weekNumber));
+
+
+
+////////  LESSON 11  ////////
+
+//Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.
+
+arrFlag = [1, 2, 3, 46, 4, 5, 6, 55];
+let flag = false;
+
+for (let i = 0; i < arrFlag.length; i++) {
+  if (arrFlag[i] == 5) {
+    flag = true;
+    break;
+  }
+}
+
+if (flag == true) {
+  console.log('yes');
+} else {
+  console.log('no');
+}
+
+console.log("NEXT");
+
+// Дано число, например 31. Проверьте, что это число не делится ни на одно другое число кроме себя самого и единицы. 
+// То есть в нашем случае нужно проверить, что число 31 не делится на все числа от 2 до 30. 
+// Если число не делится - выведите 'false', а если делится - выведите 'true'.
+
+strFlag = 25;
+let flag3 = false;
+
+for (let i = 2; i < (strFlag - 1); i++) {
+  let a = strFlag / i
+  console.log(a);
+  if (Number.isInteger(a) == true) {
+    flag3 = true;
+    break;
+  }
+}
+
+if (flag3 == true) {
+  console.log('yes');
+} else {
+  console.log('no');
+}
+
+console.log("NEXT");
+
+// Дан массив с числами. Проверьте, есть ли в нем два одинаковых числа подряд. 
+// Если есть - выведите 'да', а если нет - выведите 'нет'.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+arrFlag2Numbers = [1, 0];
+let flag2 = false;
+
+for (let i = 0; i < arrFlag2Numbers.length; i++) {
+  // if (arrFlag2Numbers[i] == 4) {
+  //   flag2 = false;
+  //   // continue;
+  // };
+  if (arrFlag2Numbers[i] == arrFlag2Numbers[0]) {
+    flag2 = true;
+    break;
+  };
+  console.log(arrFlag2Numbers[i])
+}
+
+if (flag2 == true) {
+  console.log('yes');
+} else {
+  console.log('no');
+}
+
+////////  LESSON 12  ////////
+
+// Сделайте функцию, которая параметрами принимает 2 числа. 
+// Если эти числа равны - пусть функция вернет true, а если не равны - false.
+let q = -1;
+let u = 50;
+
+function compare(q, u) {
+  if (q == u) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(compare(q, u))
+
+
+// Сделайте функцию, которая параметрами принимает 2 числа. 
+// Если их сумма больше 10 - пусть функция вернет true, а если нет - false.
+
+function sumMoreTen(q, u) {
+  if (q + u > 10) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(sumMoreTen(q, u))
+
+// Сделайте функцию, которая параметром принимает число и проверяет - 
+// отрицательное оно или нет. Если отрицательное - пусть функция вернет true, а если нет - false.
+
+function minusCheck(q) {
+  if (q < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(minusCheck(q))
+
+////////  LESSON 13  ////////
+
+// С помощью цикла for сформируйте строку '123456789' 
+// и запишите ее в переменную str.
+let strFor = '';
+
+for (let i = 1; i < 10; i++) {
+  strFor += i;                  // ПЕРЕЕННАЯ ВНУТРИ ЦИКЛА СУЩЕСТВУЕТ ТОЛЬКО ВНУТРИ! ЕСЛИ ХОТИМ ПЕРЕМЕННУЮ ДЛЯ КОДА, НУЖНО ОБЬЯВИТЬ ЕЕ ВНЕ ЦИКЛА!!! 
+}
+console.log(strFor);
+console.log(typeof strFor);
+
+// С помощью цикла for сформируйте строку '987654321' 
+// и запишите ее в переменную str.
+let strFor2 = '';
+
+for (let i = 9; i > 0; i--) {
+  strFor2 += i;
+}
+console.log(strFor2);
+
+// С помощью цикла for сформируйте строку '-1-2-3-4-5-6-7-8-9-' 
+// и запишите ее в переменную str.
+
+let strFor3 = '-' + '';
+
+for (let i = 1; i < 10; i++) {
+  strFor3 = strFor3 + i + "-";
+}
+console.log(strFor3);
+
+// Нарисуйте пирамиду, как показано на рисунке, 
+// только у вашей пирамиды должно быть 20 рядов
+let xPir = "";
+
+for (let i = 0; i < 5; i++) {
+  xPir = xPir + 'x';
+  document.write(xPir + "<br>")     // !!! СОЗДАЕТ КАК 1 22 333 4444 5555, ПЕРЕМЕННАЯ ЗАПИСЫВЕТСЯ В ПАМЯТИ
+}
+document.write('<br>')
+document.write(xPir)                // !!! СОЗДАЕТ КАК 12345
+document.write('<br>')
+document.write('<br>')
+
+// С помощью двух вложенных циклов нарисуйте следующую пирамидку:
+//1
+//22
+//333 etc.
+
+let xNum = "";
+
+for (let i = 1; i < 10; i++) {
+  for (let j = 0; j < i; j++) {  //!! j < i;
+    document.write(i);
+  }
+  document.write('<br>');
+}
+
+////////  LESSON 14  На приемы работы с массивами ////////
+
+// Заполните массив следующим образом: в первый элемент запишите 'x',
+// во второй 'xx', в третий 'xxx' и так далее.
+
+let arrX = [];
+
+for (let i = 1; i < 10; i++) {
+  arrX = [arrX + "x"];
+  console.log(arrX)
+}
+console.log(typeof arrX);
+
+// Заполните массив следующим образом: в первый элемент запишите '1', 
+// во второй '22', в третий '333' и так далее.
+
+let arrNum = [];
+
+for (let i = 1; i < 10; i++) {
+  for (let j = 0; j < i; j++) {
+    document.write(arrNum + i)
+  }
+  document.write('<br>')
+};
+
+console.log(typeof arrNum)
+
+// Сделайте функцию arrayFill, которая будет заполнять массив 
+// заданными значениями. Первым параметром функция принимает значение, 
+// которым заполнять массив, а вторым - сколько элементов должно быть в массиве. 
+// Пример: arrayFill('x', 5) сделает массив ['x', 'x', 'x', 'x', 'x']. 
+
+function arrayFill(aF, nF) {
+  let nFsum = [];
+  for (let i = 0; i < nF; i++) {
+    nFsum.push(aF)
+  }
+  return nFsum
+}
+console.log(arrayFill("x", 5))
+
+
+// Дан массив с числами. Узнайте сколько элементов с начала 
+// массива надо сложить, чтобы в сумме получилось больше 10-ти.
+
+arrSum10 = [1, 5, 6, 7.5, 8]
+let sum10 = 0;
+
+for (let i = 0; i < arrSum10.length; i++) {
+  sum10 += arrSum10[i];
+  if (sum10 > 10) {
+    console.log("We have to add " + arrSum10.indexOf(arrSum10[i + 1]) + " first numbers")
+    break;
+  } else {
+    continue;
+  }
+}
+
+// Дан массив с числами. Не используя метода reverse 
+// переверните его элементы в обратном порядке.
+let newResereArr = [];
+
+for (let i = arrSum10.length - 1; i > -1; i--) {
+  newResereArr.push(arrSum10[i]);
+}
+console.log(newResereArr)
+
+// Дан двухмерный массив с числами, например 
+// [[1, 2, 3], [4, 5], [6]]. Найдите сумму элементов этого массива. 
+// Массив, конечно же, может быть произвольным.
+
+arr2dim = [[1, 2, 3], [4, 5], [6]];
+let sumArr34 = [];
+for (let i = 0; i < arr2dim.length; i++) {
+  for (let j = 0; j < arr2dim[i].length; j++) {
+    sumArr34.push(arr2dim[i][j]);
+  }
+}
+console.log(sumArr34.reduce((sum, current) => sum + current, 0))
+
+
+// Дан трехмерный массив с числами,
+// например [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]. 
+// Найдите сумму элементов этого массива. 
+// Массив, конечно же, может быть произвольным.
+
+arr3dim = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let sumArr35 = [];
+for (let i = 0; i < arr3dim.length; i++) {
+  for (let j = 0; j < arr3dim[i].length; j++) {
+    for (let k = 0; k < arr3dim[j].length; k++) {
+      sumArr35.push(arr3dim[i][j][k]);
+    }
+  }
+};
+
+console.log(sumArr35.reduce(function(a, b){
+  return a + b;
+}, 0))
+
+//second solution
+
+let sumArr36 = 0;
+
+for (let i = 0; i < arr3dim.length; i++) {
+  for (let j = 0; j < arr3dim[i].length; j++) {
+    for (let k = 0; k < arr3dim[j].length; k++) {
+      sumArr36 += arr3dim[i][j][k];
+    }
+  }
+};
+
+console.log(sumArr36);
+
+////////  LESSON 15 Правильное использование пользовательских функций ////////
+
+// Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, 
+// что оно больше нуля и меньше 10. Если это так - пусть функция возвращает true, 
+// если не так - false.
+
+function isNumberInRange (a) {
+  if (a > 0 && a < 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isNumberInRange(1))
+
+// Дан массив с числами. Запишите в новый массив только те числа, 
+// которые больше нуля и меньше 10-ти. Для этого используйте вспомогательную 
+// функцию isNumberInRange из предыдущей задачи.
+
+arrIsRange = [1,9,50,-1,7,0,40];
+newArrIsRange = [];
+
+for (let i = 0; i < arrIsRange.length; i++) {
+  if (isNumberInRange(arrIsRange[i]) == true) {
+    newArrIsRange.push(arrIsRange[i])
+  }
+}
+
+console.log(newArrIsRange)
+
+// Сделайте функцию getDigitsSum (digit - это цифра), 
+// которая параметром принимает целое число и возвращает сумму его цифр.
+
+let primeNumber = 404;
+
+function getArr(num) {
+  return String(num).split('')               // !!!!!!!!!!! split ALWAYS WITH '' !!!!!!!!!!!!! else DON'T split!
+}
+function getDigitsSum(num){
+  num = getArr(num)         // NB!! get function getArr in this function!!
+  let numBase = 0
+  for (let i = 0; i < num.length; i++) {
+      numBase = numBase + Number(num[i])
+    }
+  return numBase
+}
+
+console.log(getDigitsSum(primeNumber));
+
+// Найдите все года от 1 до 2020, сумма цифр которых равна 13. 
+// Для этого используйте вспомогательную функцию getDigitsSum из предыдущей задачи.
+
+for (let i = 1; i <= 2020; i++) {
+  if (getDigitsSum(i) == 26) {
+    console.log(i)
+  }
+}
+
+// Сделайте функцию isEven() (even - это четный), 
+// которая параметром принимает целое число и проверяет: 
+// четное оно или нет. Если четное - пусть функция возвращает true, 
+// если нечетное - false.
+
+function isEven (num) {
+  if (num / 2 == parseInt) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isEven(4))
+// Дан массив с целыми числами. Создайте из него новый массив, 
+// где останутся лежать только четные из этих чисел. 
+// Для этого используйте вспомогательную функцию isEven из предыдущей задачи.
+
+
+
+// Сделайте функцию getDivisors, которая параметром принимает 
+// число и возвращает массив его делителей (чисел, на которое делится данное число).
+
